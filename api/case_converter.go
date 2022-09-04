88eedcd8050153129214061b82e87887caf2eaa2
cetacean <52125806+MercuryLoveShapa@users.noreply.github.com>
2022-09-04 10:47:30 +0800

    .

---
+++ api/case_converter.go
@@ -0,0 +1,17 @@
+package handler
+
+func CaseConverter()  {
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	resp := make(map[string]string)
+	resp["message"] = "Go case converter"
+	resp["language"] = "go"
+	resp["cloud"] = "Hosted on Vercel!"
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		fmt.Println("Error happened in JSON marshal. Err: %s", err)
+	} else {
+		w.Write(jsonResp)
+	}
+	return
+}
